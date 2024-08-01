826109c7b0b71777ae7e472638584825d54dfc5a
Murilo Luiz Jaboinski <143234115+muriloLuix@users.noreply.github.com>
2024-08-01 10:29:37 -0300

    Ajustando responsividade no celular

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuHamburger = document.querySelector('.menuHamburger');
     const fullScreenMenu = document.getElementById('fullScreenMenu');
-
+  
     menuHamburger.addEventListener('click', function () {
-        document.body.classList.toggle('menuOpen');
+      document.body.classList.toggle('menuOpen');
     });
-
+  
     fullScreenMenu.addEventListener('click', function (event) {
-        if (event.target.tagName === 'A' || event.target.id === 'fullScreenMenu') {
-            document.body.classList.remove('menuOpen');
-        }
+      if (event.target.tagName === 'A' || event.target.id === 'fullScreenMenu') {
+        document.body.classList.remove('menuOpen');
+      }
     });
-});
+  });
+  
