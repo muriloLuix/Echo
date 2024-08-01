d83193af3c3fa2bb79f5533ae5cc750a9c7f840b
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-08-01 02:17:00 -0300

    Ajustando responsividade em Smartphones

---
+++ assets/js/menu.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuHamburger = document.querySelector('.menuHamburger');
+    const fullScreenMenu = document.getElementById('fullScreenMenu');
+
+    menuHamburger.addEventListener('click', function () {
+        document.body.classList.toggle('menuOpen');
+    });
+
+    fullScreenMenu.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A' || event.target.id === 'fullScreenMenu') {
+            document.body.classList.remove('menuOpen');
+        }
+    });
+});
