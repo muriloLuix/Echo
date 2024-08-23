764f46b462d8ed192a4e9db551a12f5770554041
Murilo Luiz <muriloluiz654@gmail.com>
2024-08-23 17:00:48 -0300

    Recomençando echo

--- assets/js/tab.js
+++
@@ -1,9 +0,0 @@
-export function changeTabTitle() {
-    if (document.visibilityState === 'hidden') {
-        document.title = "Echo | Vai perder as ofertas?😞";
-    } else if (document.visibilityState === 'visible') {
-        document.title = "Echo | Programando o Futuro";
-    }
-}
-
-document.addEventListener('visibilitychange', changeTabTitle);
